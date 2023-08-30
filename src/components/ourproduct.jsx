40303237e8fdb627e389f258155d6bcb49c935a7
lalit patil <lalitpatil805@gmail.com>
2023-08-30 17:39:11 +0530

    change

--- src/components/ourproduct.jsx
+++ src/components/ourproduct.jsx
@@ -13,8 +13,19 @@ function Ourproduct() {
     <img style={{height:'250px', width:'450px', marginLeft:'10px'}} src={pic1} alt="pic1" />
     <img style={{height:'250px', width:'450px', marginLeft:'70px'}} src={pic2} alt="pic2" />
     <img style={{height:'250px', width:'450px',marginLeft:'60px'}} src={pic3} alt="pic3" />
-
-</div>
+</div><br/>
+<p style={{marginRight:'1050px'}}>
+    <b>Yard & Terminal Management: </b>
+    Terminal operations are highly demanding and competitive in today's world. As highly expensive equipments and infrastructure is required to manage the Terminal operations, cost and labor cost is very high. Our product give detailed view of operations across the terminal to save the time and cost of the operations including cargo arrivals and departure time management, container yard management, queue / straddle crane management.
+</p>
+<p style={{marginLeft:'3px', marginTop:'-230px'}}>
+    <b>eFile Management: </b>
+    Go Paperless. Organize , track and manage<br/> file and documents digitally with enterprise security system. Our<br/> Product offers - Automated movement of files and correspon-<br/>dences throughout the lifecycle—from creation to archival. <br/>Unified process across departments and agencies for quick<br/> file management.
+</p>
+<p style={{marginLeft:'1050px', marginTop:'-160px'}}>
+    <b>Toll Plaza Management: </b>
+    TPMS is AI system which helps to monitor the vehicles and collect information from live streaming in real time. Each and very vehicle passing through the gates has been captured and in/out timing with images and videos been captured with daily fees collection. Our systems has 95% above accuracy in capturing the details.
+</p>
 </div>
 </>
 );
